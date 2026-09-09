d2cba831bbe90168c38e6da104fc1a427c86b81d
JUNGHOON AN <gnstm831@naver.com>
2026-09-09 20:51:07 +0900

    feat: 이미지 업로드를 위한 S3 Config 파일 설정
    - S3Config 추가

---
+++ userService/src/main/java/com/algotalk/userservice/config/S3Config.java
@@ -0,0 +1,32 @@
+package com.algotalk.userservice.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+    @Value("${aws.s3.region}")
+    private String region;
+    @Value("${aws.s3.bucket}")
+    private String bucket;
+    @Value("${aws.s3.access-key}")
+    private String accessKey;
+    @Value("${aws.s3.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(() -> {
+                    return AwsBasicCredentials.create(accessKey, secretKey);
+                })
+                .build();
+    }
+
+}
